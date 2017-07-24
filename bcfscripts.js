6a861410c50b296c1b1b5e74b395f39e0fbae8c7
PointB1ank <30410855+PointB1ank@users.noreply.github.com>
2017-07-24 12:29:53 -0400

    Update bcfscripts.js

--- bcfscripts.js
+++ bcfscripts.js
@@ -1,22 +1,3 @@
 function resetFunction(){
 document.getElementById("playerinputform").reset();
 }
-
-$(function () {
-const config = {
-    apiKey: "AIzaSyDn9H15dCT237yYjpnOxtdqQUNEBxms3sQ",
-    authDomain: "bcfind-52146.firebaseapp.com",
-    databaseURL: "https://bcfind-52146.firebaseio.com",
-    projectId: "bcfind-52146",
-    storageBucket: "bcfind-52146.appspot.com",
-    messagingSenderId: "895323885188"
-  };
-  firebase.initializeApp(config);
-  $('-js-form').on('submit',event => {
-      event.preventDefault();
-      const region = $('#region').val();
-      const tier = $('#tier').val();
-      const position $('#position').val();
-      const voice $('#dota').val();
-      console.log(region,tier,position,voice);
-}
