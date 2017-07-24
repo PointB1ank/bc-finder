d31d45b118dda45635c05bfd62654e4dde7b19b7
PointB1ank <30410855+PointB1ank@users.noreply.github.com>
2017-07-24 11:01:03 -0400

    Update bcfscripts.js

--- bcfscripts.js
+++ bcfscripts.js
@@ -1,17 +1,3 @@
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDn9H15dCT237yYjpnOxtdqQUNEBxms3sQ",
-    authDomain: "bcfind-52146.firebaseapp.com",
-    databaseURL: "https://bcfind-52146.firebaseio.com",
-    projectId: "bcfind-52146",
-    storageBucket: "bcfind-52146.appspot.com",
-    messagingSenderId: "895323885188"
-  };
-  firebase.initializeApp(config);
-</script>
-
-
 function resetFunction(){
 document.getElementById("playerinputform").reset();
 }
